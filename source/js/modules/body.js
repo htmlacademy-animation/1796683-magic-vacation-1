4f1cdc7f5499776565ea96900cead6a18a4dc20c
Elisaveta Katyreva <elisaveta.katyreva@sitesoft.ru>
2021-09-03 14:52:55 +0500

    add ui modifiers to body when slider is visible

--- source/js/modules/body.js
+++ source/js/modules/body.js
@@ -1,5 +1,38 @@
 export default () => {
-  window.addEventListener('load', () => {
-    document.body.classList.add('body--loaded');
-  });  
+  let currentActiveSlideIndex = 0;
+  const modifiers = {
+    0: `body--ui-mod-1`,
+    2: `body--ui-mod-2`,
+    4: `body--ui-mod-3`,
+    6: `body--ui-mod-3`,
+  };
+
+  window.addEventListener(`load`, () => {
+    document.body.classList.add(`body--loaded`);
+  });
+
+  document.querySelector(`.js-slider`).addEventListener(`slideChanged`, (event) => {
+    currentActiveSlideIndex = event.detail.activeIndex;
+    updateUIModifiers();
+  });
+
+  document.body.addEventListener(`screenChanged`, (event) => {
+    if (event.detail.screenName === `story`) {
+      updateUIModifiers();
+    } else {
+      removeUIModifiers();
+    }
+  });
+
+  const updateUIModifiers = () => {
+    removeUIModifiers();
+    document.body.classList.add(modifiers[currentActiveSlideIndex]);
+  };
+
+  const removeUIModifiers = () => {
+    Object.keys(modifiers).forEach((key) => {
+      document.body.classList.remove(modifiers[key]);
+    });
+  };
+
 };
